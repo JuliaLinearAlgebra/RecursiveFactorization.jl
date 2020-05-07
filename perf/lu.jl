b7ac90e601249a1feaf0a4cf2c811d1c2a06ebd4
Yingbo Ma <mayingbo5@gmail.com>
2020-05-07 12:17:03 -0400

    Update the benchmark script

--- perf/lu.jl
+++ perf/lu.jl
@@ -1,31 +1,45 @@
 using BenchmarkTools
-import LinearAlgebra, RecursiveFactorization
+using LinearAlgebra, RecursiveFactorization
 
-BenchmarkTools.DEFAULT_PARAMETERS.seconds = 0.08
+BenchmarkTools.DEFAULT_PARAMETERS.seconds = 0.5
 
-luflop(m, n) = n^3÷3 - n÷3 + m*n^2
-luflop(n) = luflop(n, n)
+function luflop(m, n=m; innerflop=2)
+    sum(1:min(m, n)) do k
+        invflop = 1
+        scaleflop = isempty(k+1:m) ? 0 : sum(k+1:m)
+        updateflop = isempty(k+1:n) ? 0 : sum(k+1:n) do j
+            isempty(k+1:m) ? 0 : sum(k+1:m) do i
+                innerflop
+            end
+        end
+        invflop + scaleflop + updateflop
+    end
+end
 
 bas_mflops = Float64[]
-rec8_mflops = Float64[]
-rec16_mflops = Float64[]
-rec32_mflops = Float64[]
+rec72_mflops = Float64[]
+rec80_mflops = Float64[]
+rec256_mflops = Float64[]
+nb_mflops = Float64[]
 ref_mflops = Float64[]
-ns = 4:32:500
+ns = 4:64:600
 for n in ns
     @info "$n × $n"
     A = rand(n, n)
     bt = @belapsed LinearAlgebra.lu!($(copy(A)))
     push!(bas_mflops, luflop(n)/bt/1e9)
 
-    rt8 = @belapsed RecursiveFactorization.lu!($(copy(A)); blocksize=8)
-    push!(rec8_mflops, luflop(n)/rt8/1e9)
+    rt72 = @belapsed RecursiveFactorization.lu!($(copy(A)); threshold=72)
+    push!(rec72_mflops, luflop(n)/rt72/1e9)
 
-    rt16 = @belapsed RecursiveFactorization.lu!($(copy(A)); blocksize=16)
-    push!(rec16_mflops, luflop(n)/rt16/1e9)
+    rt80 = @belapsed RecursiveFactorization.lu!($(copy(A)); threshold=80)
+    push!(rec80_mflops, luflop(n)/rt80/1e9)
 
-    rt32 = @belapsed RecursiveFactorization.lu!($(copy(A)); blocksize=32)
-    push!(rec32_mflops, luflop(n)/rt32/1e9)
+    rt256 = @belapsed RecursiveFactorization.lu!($(copy(A)); threshold=256)
+    push!(rec256_mflops, luflop(n)/rt256/1e9)
+
+    nb = @belapsed RecursiveFactorization._generic_lufact!($(copy(A)), Val(true), Vector{Int}(undef, $n), Ref(0))
+    push!(nb_mflops, luflop(n)/nb/1e9)
 
     ref = @belapsed LinearAlgebra.generic_lufact!($(copy(A)))
     push!(ref_mflops, luflop(n)/ref/1e9)
@@ -33,9 +47,10 @@ end
 
 using Plots
 plt = plot(ns, bas_mflops, legend=:bottomright, lab="OpenBLAS", title="LU Factorization Benchmark", marker=:auto, dpi=150)
-plot!(plt, ns, rec8_mflops, lab="RF8", marker=:auto)
-plot!(plt, ns, rec16_mflops, lab="RF16", marker=:auto)
-plot!(plt, ns, rec32_mflops, lab="RF32", marker=:auto)
+plot!(plt, ns, rec72_mflops, lab="RF72", marker=:auto)
+plot!(plt, ns, rec80_mflops, lab="RF80", marker=:auto)
+plot!(plt, ns, rec80_mflops, lab="RF256", marker=:auto)
+plot!(plt, ns, nb_mflops, lab="No recursion", marker=:auto)
 plot!(plt, ns, ref_mflops, lab="Reference", marker=:auto)
 xaxis!(plt, "size (N x N)")
 yaxis!(plt, "GFLOPS")
