1c971060143cac99aebad7eff5d2d135421b95d3
Yingbo Ma <mayingbo5@gmail.com>
2021-06-30 11:48:27 -0400

    More coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,8 @@ end
 @testset "Test LU factorization" begin
     for _p in (true, false), T in (Float64, Float32, ComplexF64, ComplexF32, Real)
         p = Val(_p)
-        for s in [1:10; 50:80:200; 300]
+        for (i, s) in enumerate([1:10; 50:80:200; 300])
+            iseven(i) && (p = RecursiveFactorization.to_stdlib_pivot(p))
             siz = (s, s+2)
             @info("size: $(siz[1]) × $(siz[2]), T = $T, p = $_p")
             if isconcretetype(T)
